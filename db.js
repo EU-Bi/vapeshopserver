27f942165760091a64356a86b9b961f192c59afe
Евгений Билан <evgenebilan@MacBook-Air-Evgenij.local>
2023-07-11 20:02:20 +0300

    new db

--- db.js
+++ db.js
@@ -1,17 +1,18 @@
 const { Sequelize } = require("sequelize");
 const pg = require("pg");
 
-module.exports = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: "postgres",
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-  }
-  //   process.env.POSTGRES_URL + "?sslmode=require",
-  //   {
-  //     dialectModule: pg,
-  //   }
+module.exports = new Sequelize(process.env.POSTGRES_URL+"?sslmode=require",{dialectModule: pg}
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       dialect: "postgres",
+//       host: process.env.DB_HOST,
+//       port: process.env.DB_PORT,
+//     }
+  
+//   {
+//     ,
+    // host:process.env.DB_HOST,
+    // port: process.env.DB_PORT,
 );
